127000a5feab946192250d118100a47bd5baaa2a
qa-jjltp-1-djuk@example.com <qa-jjltp-1-djuk@example.com>
2023-04-05 18:49:09 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjE2MTE0Mw/spectral.js?branch_slug=main&token=ab9f7276-6a4e-4649-9d2f-b0cf27d09d66";
+export default { extends: ruleset };
